36e43ed743b7c0262559acd32678effa02ebee58
Wang <wrc582029763@gmail.com>
2023-10-23 17:02:10 +0800

    posix_2

---
+++ posix/src/bin/read.rs
@@ -0,0 +1,33 @@
+// 这是从共享内存中读取数据的代码
+use std::ffi::CString;
+use std::os::unix::prelude::FromRawFd;
+use std::fs::File;
+use nix::sys::mman::{shm_open, munmap, mmap, MapFlags, ProtFlags};
+use nix::fcntl::OFlag;
+use nix::sys::stat::Mode;
+use std::ptr::null_mut;
+use std::mem::size_of;
+use std::io::Read;
+
+fn main() {
+    let shared_name = CString::new("/test2.shm").expect("CString::new failed");
+    let name_c_str = shared_name.as_c_str();
+    let mem_size: usize = size_of::<i32>();
+
+    // Open the existing shared memory and get the file descriptor
+    let fd = shm_open(name_c_str, OFlag::O_RDWR, Mode::empty()).expect("shm_open failed");
+
+    // Map the shared memory into the address space of this process
+    let ptr = unsafe { mmap(null_mut(), mem_size, ProtFlags::PROT_READ, MapFlags::MAP_SHARED, fd, 0).expect("mmap failed") };
+
+    // Operate on the shared memory
+    let mut file = unsafe { File::from_raw_fd(fd) };
+
+    let mut buffer = [0; 4];
+    file.read(&mut buffer).expect("read failed");
+    let val = i32::from_ne_bytes(buffer);
+    assert_eq!(val, 100);
+
+    // This process finished using this shared memory, so do unmap
+    unsafe { munmap(ptr, mem_size).expect("munmap failed") };
+}
--- posix/src/main.rs
+++ posix/src/main.rs
@@ -1,4 +1,5 @@
-use nix::fcntl::OFlag;//ftruncate 当前nix 中没有
+// use nix::fcntl::OFlag;//ftruncate 当前nix 中没有
+use nix::fcntl::{OFlag, ftruncate};
 use nix::sys::mman::{mmap, munmap, shm_open, shm_unlink, MapFlags, ProtFlags};
 use nix::sys::stat::Mode;
 use std::ffi::{CStr, CString};
@@ -16,10 +17,10 @@ fn main() {
     let c_str_name: &CStr = name.as_c_str();
     let fd = shm_open(c_str_name, OFlag::O_CREAT | OFlag::O_RDWR, Mode::empty()).unwrap();
 
-    unsafe {
-        // let _ = ftruncate(fd, mem_size as _);
-        libc::ftruncate(fd, mem_size as _);
-    }
+    let _ = ftruncate(fd, mem_size as _);
+    // unsafe {
+    //     libc::ftruncate(fd, mem_size as _);
+    // }
 
     let ptr = unsafe {
         mmap(
