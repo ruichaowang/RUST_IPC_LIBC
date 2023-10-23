59adbd29619ced668a9966d15188d84b53621824
Wang <wrc582029763@gmail.com>
2023-10-23 14:30:23 +0800

    need su

--- mem_producer/src/main.rs
+++ mem_producer/src/main.rs
@@ -1,7 +1,12 @@
 extern crate libc;
-use std::ffi::CString;
+use std::any::Any;
 use std::ptr;
+use std::result;
 use std::slice;
+use std::mem::ManuallyDrop;
+use std::panic;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 use libc::sleep;
 
@@ -12,6 +17,7 @@ struct Shm {
 
 impl Drop for Shm {
     fn drop(&mut self) {
+        println!("Shared memory closed");
         unsafe {
             // detaching the shared memory segment
             if libc::shmdt(self.addr) == -1 {
@@ -29,28 +35,50 @@ impl Drop for Shm {
 fn main() {
     println!("Opening shared memory...");
     let key = 5678;
-    let shmid: i32;
-    let shm: *mut libc::c_void;
-    unsafe {
-        shmid = libc::shmget(key, 46 , libc::IPC_CREAT | 0666);
-        if shmid == -1 {
-            panic!("shmget failed: {}", std::io::Error::last_os_error());
+    let shm_obj = Arc::new(Mutex::new(None));
+    
+    let result = panic::catch_unwind(|| {
+        let shmid: i32;
+        let shm: *mut libc::c_void;
+        unsafe {
+            shmid = libc::shmget(key, 46 , libc::IPC_CREAT | 0666);
+            if shmid == -1 {
+                panic!("shmget failed: {}", std::io::Error::last_os_error());
+            }
+            shm = libc::shmat(shmid, ptr::null(), 0);
+            if shm as isize == -1 {
+                panic!("shmat failed: {}", std::io::Error::last_os_error());
+            }
         }
-        shm = libc::shmat(shmid, ptr::null(), 0);
-        if shm as isize == -1 {
-            panic!("shmat failed: {}", std::io::Error::last_os_error());
-        }
+        let shm_obj_inner = Shm {
+            id: shmid,
+            addr: shm,
+        };
+
+        let mut guard = shm_obj.lock().unwrap();
+        let _ = std::mem::replace(&mut *guard, Some(shm_obj_inner)); // 锁定 Mutex，并设置共享数据
+
+
+        let hello_world = b"this is the text we want to share through IPC\n";
+        let slice = {
+            let shm_addr = guard.as_mut().unwrap().addr as *mut u8; // 使用 guard 获取共享数据
+            unsafe { std::slice::from_raw_parts_mut(shm_addr, hello_world.len()) }
+        };
+        slice.copy_from_slice(&hello_world[..]);
+
+    });
+
+    match result {
+        Ok(_) => {
+            let shm_obj_inner = shm_obj.lock().unwrap().take().unwrap(); // 锁定 Mutex 并获取共享的数据
+            // 在这里可以使用 shm_obj_inner 进行后续操作
+        },
+        Err(_) => {
+            println!("Shared memory failed to open");
+            drop(shm_obj);
+            return;
+        },
     }
-    let shm_obj = Shm {
-        id: shmid,
-        addr: shm,
-    };
 
-
-    let hello_world = b"this is the text we want to share through IPC\n";
-    let slice = unsafe { slice::from_raw_parts_mut(shm_obj.addr as *mut u8, hello_world.len()) };
-    slice.copy_from_slice(&hello_world[..]);
-
-    unsafe { sleep(20) };
-    println!("closing shared memory...");
+    unsafe { sleep(10) };
 }
