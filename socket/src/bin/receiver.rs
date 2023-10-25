1305488bd97c3f66d0ef95280880719a4b9d3354
Wang <wrc582029763@gmail.com>
2023-10-25 18:26:45 +0800

    socket work

--- socket/src/bin/receiver.rs
+++ socket/src/bin/receiver.rs
@@ -1,21 +1,17 @@
 use byteorder::{LittleEndian, ReadBytesExt};
-use std::fs;
-use std::fs::File;
 use std::os::unix::net::UnixListener;
 
 fn main() {
-    // 删除旧的套接字，如果存在的话
-    fs::remove_file("/tmp/socket.sock").unwrap();
-    //在初始化监听之前，请确保套接字文件已经创建并存在于正确的路径上。
-    File::create("/tmp/socket.sock").expect("Could not create socket file");
-
-    let listener = UnixListener::bind("/tmp/socket.sock").expect("failed to bind socket");
+    let path = "/system/bin/socket.sock";
+    let listener = UnixListener::bind(path).expect("failed to bind socket");
     for stream in listener.incoming() {
         let mut stream = stream.expect("failed to accept connection");
         let number = stream
             .read_i32::<LittleEndian>()
             .expect("Failed to read integer from stream");
         println!("Received number: {}", number);
+        std::fs::remove_file(path).unwrap();  // 删除旧的套接字，如果存在的话
+
         break;
     }
 }
--- socket/src/bin/sender.rs
+++ socket/src/bin/sender.rs
@@ -1,11 +1,8 @@
 use std::os::unix::net::UnixStream;
 use byteorder::{WriteBytesExt, LittleEndian};
-use std::io::Write;
-use std::fs;
 
 fn main() {
-    fs::remove_file("/tmp/socket.sock").unwrap();  // 删除旧的套接字，如果存在的话
-
-    let mut stream = UnixStream::connect("/tmp/socket.sock").expect("failed to connect to socket");
-    stream.write_i32::<LittleEndian>(127).expect("Failed to write integer to stream");
+    let path = "/system/bin/socket.sock";
+    let mut stream = UnixStream::connect(path).expect("failed to connect to socket");
+    stream.write_i32::<LittleEndian>(12).expect("Failed to write integer to stream");
 }
