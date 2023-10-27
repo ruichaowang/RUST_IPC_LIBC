56fa76da0a75cd4e987afbdd546d47c1283c6a18
Wang <wrc582029763@gmail.com>
2023-10-27 11:55:43 +0800

    socket with fd crash

--- socket/src/bin/receiver.rs
+++ socket/src/bin/receiver.rs
@@ -1,17 +1,54 @@
-use byteorder::{LittleEndian, ReadBytesExt};
-use std::os::unix::net::UnixListener;
+use std::os::unix::net::{UnixListener, UnixStream};
+use std::os::unix::io::{AsRawFd, RawFd};
+use std::path::Path;
+use libc::{c_int, c_void, msghdr, iovec, sendmsg, recvmsg, c_uint, SOL_SOCKET, SCM_RIGHTS, CMSG_LEN, CMSG_DATA};
+
+fn recv_fd(sock: &UnixStream) -> std::io::Result<RawFd> {
+    // 接收消息
+    let mut buf = [0u8; 1024];
+    let mut iov = iovec {
+        iov_base: buf.as_mut_ptr() as *mut c_void,
+        iov_len: buf.len(),
+    };
+    let mut hdr = msghdr {
+        msg_name: std::ptr::null_mut(),
+        msg_namelen: 0,
+        msg_iov: &mut iov,
+        msg_iovlen: 1,
+        msg_control: std::ptr::null_mut(),
+        msg_controllen: 0,
+        msg_flags: 0,
+    };
+    let ret = unsafe { recvmsg(sock.as_raw_fd(), &mut hdr, 0) };
+    if ret < 0 {
+        return Err(std::io::Error::last_os_error());
+    }
+
+    // 解析消息
+    let cmsg = unsafe { libc::CMSG_FIRSTHDR(&hdr) };
+    let fd_ptr = unsafe { libc::CMSG_DATA(cmsg) as *const c_int };
+    let fd = unsafe { std::ptr::read(fd_ptr) };
+    Ok(fd)
+}
+
 
 fn main() {
-    let path = "/system/bin/socket.sock";
-    let listener = UnixListener::bind(path).expect("failed to bind socket");
-    for stream in listener.incoming() {
-        let mut stream = stream.expect("failed to accept connection");
-        let number = stream
-            .read_i32::<LittleEndian>()
-            .expect("Failed to read integer from stream");
-        println!("Received number: {}", number);
-        std::fs::remove_file(path).unwrap();  // 删除旧的套接字，如果存在的话
-
-        break;
+    // let path = "/system/bin/socket.sock";
+    let path = "/tmp/socket.sock";
+    if Path::new(&path).exists() {
+        std::fs::remove_file(path).expect("Failed to remove file");
     }
+
+    let listener = UnixListener::bind(path).expect("failed to bind socket");
+    let (sock, _) = listener.accept().expect("accept error");
+    let fd = recv_fd(&sock).expect("Failed to receive fd"); 
+    println!("Received fd: {}", fd);
+
 }
+
+
+
+        // let number = stream
+        //     .read_i32::<LittleEndian>()
+        //     .expect("Failed to read integer from stream");
+        // println!("Received number: {}", number);
--- socket/src/bin/sender.rs
+++ socket/src/bin/sender.rs
@@ -1,8 +1,59 @@
-use std::os::unix::net::UnixStream;
-use byteorder::{WriteBytesExt, LittleEndian};
+use std::fs::File;
+use std::os::fd::IntoRawFd;
+use std::os::unix::net::{UnixListener, UnixStream};
+use std::os::unix::io::{AsRawFd, RawFd};
+use libc::{c_int, c_void, msghdr, iovec, sendmsg, recvmsg, c_uint, SOL_SOCKET, SCM_RIGHTS, CMSG_LEN, CMSG_DATA};
+
+
+
+
+fn send_fd(fd: RawFd, stream: &mut UnixStream) -> std::io::Result<()> {
+    // 准备发送的消息
+    let mut iov = iovec {
+        iov_base: std::ptr::null_mut(),
+        iov_len: 0,
+    };
+    let mut hdr = msghdr {
+        msg_name: std::ptr::null_mut(),
+        msg_namelen: 0,
+        msg_iov: &mut iov,
+        msg_iovlen: 1,
+        msg_control: std::ptr::null_mut(),
+        msg_controllen: 0,
+        msg_flags: 0,
+    };
+    let cmsg_space = unsafe { libc::CMSG_SPACE(std::mem::size_of::<c_int>() as c_uint) as usize };
+    let mut buf = vec![0u8; cmsg_space];
+    let cmsg = unsafe { libc::CMSG_FIRSTHDR(&mut hdr) };
+    unsafe {
+        if !cmsg.is_null() {
+            (*cmsg).cmsg_level = SOL_SOCKET;
+            (*cmsg).cmsg_type = SCM_RIGHTS;
+            (*cmsg).cmsg_len = CMSG_LEN(std::mem::size_of::<c_int>() as u32);
+            let fd_ptr = CMSG_DATA(cmsg) as *mut c_int;
+            std::ptr::write(fd_ptr, fd);
+        }
+    }
+    hdr.msg_control = buf.as_mut_ptr() as *mut c_void;
+    hdr.msg_controllen = buf.len() as u32;
+
+    // 发送消息
+    let ret = unsafe { sendmsg(stream.as_raw_fd(), &hdr, 0) };
+    if ret < 0 {
+        return Err(std::io::Error::last_os_error());
+    }
+
+    Ok(())
+}
+
 
 fn main() {
-    let path = "/system/bin/socket.sock";
+    let path = "/tmp/socket.sock";
+    let file = File::open("/Users/wangruichao/Downloads/W01_fake_no_ext.zip").expect("Failed to open file");
+    let fd = file.into_raw_fd();
     let mut stream = UnixStream::connect(path).expect("failed to connect to socket");
-    stream.write_i32::<LittleEndian>(12).expect("Failed to write integer to stream");
+    // send_fd(fd, &mut stream).expect("failed to send fd");
 }
+
+
+    // stream.write_i32::<LittleEndian>(12).expect("Failed to write integer to stream");
