f168c1263f617309084fb99a7ab650d4e3355846
wangruichao <wangruichao@lixiang.com>
2023-10-31 14:42:47 +0800

    able to run in linux

--- socket/src/bin/receiver.rs
+++ socket/src/bin/receiver.rs
@@ -1,54 +1,84 @@
 use byteorder::{ReadBytesExt, LittleEndian};
 use libc::{c_int, c_void, iovec, msghdr, recvmsg};
+use std::os::fd;
 use std::os::unix::io::{AsRawFd, RawFd};
 use std::os::unix::net::{UnixListener, UnixStream};
 use std::path::Path;
+use std::thread;
+use std::time::Duration;
 
 ///
-fn recv_fd(sock: &UnixStream) -> std::io::Result<RawFd> {
-    // 接收消息
-    let mut buf = [0u8; 16];
+fn recv_fd(sock: &mut UnixStream) -> std::io::Result<RawFd> {
+    let mut buffer = [0u8; 1];
+    let mut iov = iovec { iov_base: buffer.as_mut_ptr() as *mut c_void, iov_len: 1 };
 
-    let mut hdr = msghdr {
+    let mut buf: [u8; 16] = [0; 16];
+    let cmsg_space = unsafe { libc::CMSG_SPACE(std::mem::size_of::<c_int>() as u32) } as usize;
+    
+    let mut msg = msghdr {
         msg_name: std::ptr::null_mut(),
         msg_namelen: 0,
-        msg_iov: std::ptr::null_mut(),
-        msg_iovlen: 0,
+        msg_iov: &mut iov,
+        msg_iovlen: 1,
         msg_control: buf.as_mut_ptr() as *mut c_void,
-        msg_controllen: buf.len(), //as u32
+        msg_controllen: cmsg_space,
         msg_flags: 0,
     };
 
-    let ret = unsafe { recvmsg(sock.as_raw_fd(), &mut hdr, 0) };
-    if ret == -1 {
-        return Err(std::io::Error::last_os_error());
-    }
-
-    // 解析消息
-    let cmsg = unsafe { libc::CMSG_FIRSTHDR(&hdr) };
-    if cmsg.is_null() {
-        println!("No file descriptor received");
-        return Err(std::io::Error::new(std::io::ErrorKind::Other, "No file descriptor received"));
+    let ret = unsafe { recvmsg(sock.as_raw_fd(), &mut msg, 0) };
+    if ret <= 0 {
+        Err(std::io::Error::last_os_error())
     } else {
-        println!("File descriptor received");
+        let cmsg = unsafe { libc::CMSG_FIRSTHDR(&mut msg) };
+        if cmsg.is_null() {
+            Err(std::io::Error::new(std::io::ErrorKind::Other, "No file descriptor received"))
+        } else {
+            Ok(unsafe { *(libc::CMSG_DATA(cmsg) as *mut c_int) })
+        }
     }
+}
 
-    let fd_ptr = unsafe { libc::CMSG_DATA(cmsg) as *const c_int };
-    let fd = unsafe { std::ptr::read(fd_ptr) };
-    println!("4");
-    Ok(fd)
+
+fn print_waiting() {
+    for _ in 1..6 { // 5秒的总等待时间, 修改这个范围以改变等待时间
+        println!("Waiting for incoming connection...");
+        thread::sleep(Duration::from_secs(1)); //线程休眠1秒
+    }
 }
 
 
 fn main() {
-    let path = "/system/bin/socket.sock";
-    // let path = "/tmp/socket.sock";
+    // let path = "/system/bin/socket.sock";
+    let path = "/tmp/socket.sock";
     if Path::new(&path).exists() {
         std::fs::remove_file(path).expect("Failed to remove file");
     }
 
     let listener = UnixListener::bind(path).expect("failed to bind socket");
-    let (sock, _) = listener.accept().expect("accept error");
-    let fd = recv_fd(&sock).expect("Failed to receive fd");
-    println!("Received fd: {}", fd);
+
+    loop {
+        print_waiting(); // 打印提示
+        match listener.accept() {
+          Ok((mut sock, _)) => {
+              let fd = recv_fd(&mut sock);
+            //   let fd = sock.read_i32::<LittleEndian>();
+              match fd {
+                  Ok(fd) => {
+                      println!("Successfully received fd: {}", fd);
+                      //添加你的文件操作代码...
+  
+                   // 一旦我们成功地从一个连接中接收到文件，我们可以跳出循环
+                      break;
+                  },
+                  Err(e) => {
+                      println!("Failed to receive fd: {}", e);
+                      break;
+                  }
+              }                    
+          },
+          Err(err) => {
+              println!("Failed to accept socket connection: {}", err);
+          }
+        }
+      }
 }
--- socket/src/bin/sender.rs
+++ socket/src/bin/sender.rs
@@ -1,7 +1,7 @@
 
 use byteorder::{WriteBytesExt, LittleEndian};
 use libc::{
-    c_int, c_uint, c_void, msghdr, sendmsg, CMSG_DATA, CMSG_LEN, SCM_RIGHTS, SOL_SOCKET,
+    c_int, c_uint, c_void, msghdr, sendmsg, CMSG_DATA, CMSG_LEN, SCM_RIGHTS, SOL_SOCKET, iovec,
 };
 use std::fs::File;
 use std::os::fd::IntoRawFd;
@@ -10,56 +10,39 @@ use std::os::unix::net::UnixStream;
 use std::thread::sleep;
 use std::time::Duration;
 
-/// 发送文件描述符，但是这个方法有问题，接收方接收到的文件描述符空的
+/// 发送文件描述符
 fn send_fd(fd: RawFd, stream: &mut UnixStream) -> std::io::Result<()> {
-    // 准备发送的消息
-    let cmsg_space = unsafe { libc::CMSG_SPACE(std::mem::size_of::<c_int>() as c_uint) as usize };
-    println!("Control message space: {}", cmsg_space);
+    let mut buffer = [0u8; 1];
+    let mut iov = iovec { iov_base: buffer.as_mut_ptr() as *mut c_void, iov_len: 1 };
 
-    let mut buf: [u8; 16] = [0; 16]; // Assuming 128 bytes is large enough to accommodate control message.
-    let mut hdr = msghdr {
+    let mut buf: [u8; 16] = [0; 16];
+    let cmsg_space = unsafe { libc::CMSG_SPACE(std::mem::size_of::<c_int>() as u32) } as usize;
+    
+    let mut msg = msghdr {
         msg_name: std::ptr::null_mut(),
         msg_namelen: 0,
-        msg_iov: std::ptr::null_mut(),
-        msg_iovlen: 0,
+        msg_iov: &mut iov,
+        msg_iovlen: 1,
         msg_control: buf.as_mut_ptr() as *mut c_void,
-        msg_controllen: cmsg_space, //as u32
+        msg_controllen: cmsg_space,
         msg_flags: 0,
     };
 
-    let cmsg = unsafe { libc::CMSG_FIRSTHDR(&mut hdr) };
-    if cmsg.is_null() {
-        println!("No control message found");
-        return Err(std::io::Error::new(std::io::ErrorKind::Other, "No control message found"));
-    } else {
-        println!("Control message found");
-    }
-
-    println!("2");
-
+    let cmsg = unsafe { libc::CMSG_FIRSTHDR(&mut msg) };
     unsafe {
-        if !cmsg.is_null() {
-            (*cmsg).cmsg_level = SOL_SOCKET;
-            (*cmsg).cmsg_type = SCM_RIGHTS;
-            (*cmsg).cmsg_len = CMSG_LEN((std::mem::size_of::<c_int>() as usize).try_into().unwrap()) as usize; //as u32
-            let fd_ptr = CMSG_DATA(cmsg) as *mut c_int;
-            std::ptr::write(fd_ptr, fd);
-        }
+        (*cmsg).cmsg_level = SOL_SOCKET;
+        (*cmsg).cmsg_type = SCM_RIGHTS;
+        (*cmsg).cmsg_len = libc::CMSG_LEN(std::mem::size_of::<c_int>() as u32) as usize;
+        *(libc::CMSG_DATA(cmsg) as *mut c_int) = fd;
     }
 
-    println!("3");
-
-    // 发送消息
-    let ret = unsafe { sendmsg(stream.as_raw_fd(), &hdr, 0) };
+    let ret = unsafe { sendmsg(stream.as_raw_fd(), &mut msg, 0) };
     if ret < 0 {
-        return Err(std::io::Error::last_os_error());
+        Err(std::io::Error::last_os_error())
+    } else {
+        Ok(())
     }
-
-    println!("4");
-
-    Ok(())
 }
-
 fn is_fd_valid(fd: RawFd) {
     let ret = unsafe { libc::fcntl(fd, libc::F_GETFL) };
     if ret == -1 {
@@ -70,12 +53,13 @@ fn is_fd_valid(fd: RawFd) {
 }
 
 fn main() {
-    let path = "/system/bin/socket.sock";
-    let file = File::open("/system/bin/test.txt")
+    let path = "/tmp/socket.sock";     // let path = "/system/bin/socket.sock";
+    let file = File::open("/mnt/data/RUST_IPC_LIBC/socket/src/bin/testfile.txt")
         .expect("Failed to open file");
     let fd = file.into_raw_fd();
-    is_fd_valid(fd);
+    // is_fd_valid(fd);
     let mut stream = UnixStream::connect(path).expect("failed to connect to socket");
     send_fd(fd, &mut stream).expect("failed to send fd");
-    sleep(Duration::from_secs(20));
+    // stream.write_i32::<LittleEndian>(fd).expect("Failed to write integer to stream");
+    // sleep(Duration::from_secs(10));
 }
